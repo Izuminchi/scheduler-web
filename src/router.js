d6849f2d67f310d28089127c34b4613b1ff5cb0d
Izumi Yokouchi <hummohhu@gmail.com>
2019-02-11 15:51:44 +0900

    delete

--- src/router.js
+++ src/router.js
@@ -3,9 +3,6 @@ import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Login from './views/Login.vue'
 import Signup from './views/Signup.vue'
-import ForgotPassword from './views/ForgotPassword.vue'
-import Success from './views/Success.vue'
-import ResetPassword from './views/ResetPassword.vue'
 
 Vue.use(Router);
 
@@ -27,21 +24,6 @@ export default new Router({
       path: '/signup',
       name: 'signup',
       component: Signup
-    },
-    {
-      path: '/forgotpassword',
-      name: 'forgotpassword',
-      component: ForgotPassword
-    },
-    {
-      path: '/success',
-      name: 'success',
-      component: Success
-    },
-    {
-      path: '/resetpassword',
-      name: 'resetpassword',
-      component: ResetPassword
     }
   ]
 });
