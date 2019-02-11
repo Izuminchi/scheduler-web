e8e8bfa19dd1a922839da055a523f0d0ca547f60
Izumi Yokouchi <hummohhu@gmail.com>
2019-02-11 15:42:01 +0900

    implement signup view

--- src/router.js
+++ src/router.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Login from './views/Login.vue'
+import Signup from './views/Signup.vue'
+import ForgotPassword from './views/ForgotPassword.vue'
+import Success from './views/Success.vue'
+import ResetPassword from './views/ResetPassword.vue'
 
 Vue.use(Router);
 
@@ -18,6 +22,26 @@ export default new Router({
       path: '/login',
       name: 'login',
       component: Login
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: Signup
+    },
+    {
+      path: '/forgotpassword',
+      name: 'forgotpassword',
+      component: ForgotPassword
+    },
+    {
+      path: '/success',
+      name: 'success',
+      component: Success
+    },
+    {
+      path: '/resetpassword',
+      name: 'resetpassword',
+      component: ResetPassword
     }
   ]
 });
---
+++ src/views/Signup.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <p>test</p>
+    <input v-model="name" name="name" type="text">
+    <input v-model="email" name="email" type="email">
+    <input v-model="password" name="password" type="password">
+    <input v-model="password_confirm" name="password_confirm" type="password">
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        name: '',
+        email: '',
+        password: '',
+        password_confirm: '',
+      };
+    },
+    methods: {
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
